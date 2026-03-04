37388f5f2d3cf6c57217e7e2cc55fda20db0d4ba
jothijarvis <158240576+jothijarvis@users.noreply.github.com>
2026-03-04 18:18:53 +0530

    Update func-finding_area_and_volume.py

--- func-finding_area_and_volume.py
+++ func-finding_area_and_volume.py
@@ -1,5 +1,39 @@
 # Find area and volume. author: Jothiswarooban
-def area_volume_calc(inputfile):
+def area_calc(x1,x2,x3,y1,y2,y3,z1,z2,z3):
+    AB = [x2-x1,y2-y1,z2-z1] 
+    BC = [x3-x1,y3-y1,z3-z1] 
+    i1,j1,k1 = AB ; i2,j2,k2 = BC ; 
+    i = j1*k2 - k1*j2
+    j = i1*k2 - i2*k1
+    k = i1*j2 - i2*j1
+
+    cross_mag = (i*i + j*j + k*k) ** 0.5
+    area = 0.5 * cross_mag
+    return area
+def volume_calc(triangles,normals):
+
+    total_area = 0 ;  total_volume = 0.0; 
+    for i in range(len(triangles)):
+
+        (x1,y1,z1),(x2,y2,z2),(x3,y3,z3) = triangles[i]
+        
+        nx = normals[i][0]
+        ny = normals[i][1]
+        nz = normals[i][2]
+
+        cx = (x1 + x2 + x3) / 3.0
+        cy = (y1 + y2 + y3) / 3.0
+        cz = (z1 + z2 + z3) / 3.0
+
+        area = area_calc(x1,x2,x3,y1,y2,y3,z1,z2,z3)
+        total_area = area+total_area
+        total_volume = total_volume + (cx*nx + cy*ny + cz*nz) * area
+    if total_volume < 0:
+        total_volume = -total_volume
+
+    total_volume = total_volume / 3.0
+    return total_area,total_volume
+def get_trias_normals(inputfile):
 
     with open(inputfile) as file:
         stl_text = file.read()
@@ -26,39 +60,13 @@ def area_volume_calc(inputfile):
             if len(current_triangle) == 3:
                 triangles.append(current_triangle)
                 normals.append(current_normal)
+    return triangles,normals
 
-    total_area = 0 ;  total_volume = 0.0; 
-    for i in range(len(triangles)):
-
-        (x1,y1,z1),(x2,y2,z2),(x3,y3,z3) = triangles[i]
-        
-        nx = normals[i][0]
-        ny = normals[i][1]
-        nz = normals[i][2]
-
-        cx = (x1 + x2 + x3) / 3.0
-        cy = (y1 + y2 + y3) / 3.0
-        cz = (z1 + z2 + z3) / 3.0
-
-        AB = [x2-x1,y2-y1,z2-z1] 
-        BC = [x3-x1,y3-y1,z3-z1] 
-        i1,j1,k1 = AB ; i2,j2,k2 = BC ; 
-        i = j1*k2 - k1*j2
-        j = i1*k2 - i2*k1
-        k = i1*j2 - i2*j1
-
-        cross_mag = (i*i + j*j + k*k) ** 0.5
-        area = 0.5 * cross_mag
-        total_area = area+total_area
-        total_volume = total_volume + (cx*nx + cy*ny + cz*nz) * area
-    if total_volume < 0:
-        total_volume = -total_volume
-
-    total_volume = total_volume / 3.0
-    return total_area,total_volume
 def main():
-    f = 'D:/pytraining/box.stl'
-    total_area,total_volume = area_volume_calc(f)
+    '''Pass the location of stl file, and the geometry should have trias in it '''
+    file = 'D:/pytraining/box.stl'
+    triangles,normals = get_trias_normals(file)
+    total_area,total_volume = volume_calc(triangles,normals)
     print("Total volume:",total_volume)
     print("Total area:",total_area)
 main()
